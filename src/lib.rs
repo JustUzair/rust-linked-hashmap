b1d2892c5f8d653e42cf99250f845e4ba6b67743
JustUzair <justuzairsaiyed@gmail.com>
2026-01-22 03:01:23 +0530

    feat: updated resize logic to allow draining and rebuilding of buckets

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::mem;
 use std::{
     collections::hash_map::DefaultHasher,
     hash::{Hash, Hasher},
@@ -7,12 +8,14 @@ const INITIAL_NBUCKETS: usize = 1;
 
 pub struct HashMap<K, V> {
     buckets: Vec<Vec<(K, V)>>,
+    items: usize,
 }
 
 impl<K, V> HashMap<K, V> {
     pub fn new() -> Self {
         HashMap {
             buckets: Vec::new(),
+            items: 0,
         }
     }
 }
@@ -21,16 +24,11 @@ impl<K, V> HashMap<K, V>
 where
     K: Hash + Eq,
 {
-    fn resize(&mut self) {
-        let target_buckets = match self.buckets.len() {
-            0 => INITIAL_NBUCKETS,
-            n => n * 2,
-        };
-
-        todo!("more functionality ");
-    }
-
     pub fn insert(&mut self, key: K, value: V) -> Option<V> {
+        if self.buckets.is_empty() || self.items >= self.buckets.len() / 3 {
+            self.resize();
+        }
+
         let mut hasher = DefaultHasher::new();
         // feed value of key into the hasher
         key.hash(&mut hasher);
@@ -38,15 +36,33 @@ where
         let bucket: usize = (hasher.finish() % self.buckets.len() as u64) as usize;
         let bucket = &mut self.buckets[bucket];
 
+        self.items += 1;
         for &mut (ref ekey, ref mut evalue) in bucket.iter_mut() {
             if ekey == &key {
-                use std::mem;
                 return Some(mem::replace(evalue, value));
             }
         }
         bucket.push((key, value));
 
         None
+    }
+    fn resize(&mut self) {
+        let target_size = match self.buckets.len() {
+            0 => INITIAL_NBUCKETS,
+            n => n * 2,
+        };
+
+        let mut new_buckets = Vec::with_capacity(target_size);
+        new_buckets.extend((0..target_size).map(|_| Vec::new()));
+        for (key, value) in self.buckets.iter_mut().flat_map(|bucket| bucket.drain(..)) {
+            let mut hasher = DefaultHasher::new();
+            // feed value of key into the hasher
+            key.hash(&mut hasher);
+            // finish the hasher to get the final hash value
+            let bucket: usize = (hasher.finish() % new_buckets.len() as u64) as usize;
+            new_buckets[bucket].push((key, value));
+        }
+        let _ = mem::replace(&mut self.buckets, new_buckets);
     }
 }
 
